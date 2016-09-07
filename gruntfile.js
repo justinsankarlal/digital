06730a38356ab6d02d786007a4b67fa9fc8c7e9c
Narandiran Lakshmipathy <narandiran.lakshmipathy@astrazeneca.com>
2016-09-07 10:54:33 +0530

    updating files

--- gruntfile.js
+++ gruntfile.js
@@ -1,45 +1,30 @@
-var path = require('path');
+/*build commands
+    devlopment build --> grunt || grunt dev
+    watch --> grunt watch
+    production build --> grunt prod
+*/
+
 module.exports = function(grunt){
 	grunt.initConfig({
 		uglify: {
 			dist: {
 				files: {
-					'dist/js/edu-custom.js': 'dist/js/edu-custom.js',
-					'dist/js/edu-lib.js' : 'dist/js/edu-lib.js'
+					'build/js/multibrand.js': 'build/js/multibrand.js',
 				}
 			}
 		},
 		concat : {
 			dist : {
 				files: {
-					'dist/js/edu-lib.js' : [
-						/*'js/lib/jquery.js',
-						'js/lib/bootstrap.js',*/
-						'js/lib/angular.js',
-						'js/lib/angular-ui-router.js'
-					],
-					'dist/js/edu-custom.js': [
-						'js/src/app.js',
-						'js/src/config.js',
-						'js/src/services/*.js',
-						'js/src/controllers/*.js'
+					'build/js/multibrand.js': [
+						'js/lib/*.js',
+						'js/*.js'
 					]
 				}
 			}
 		},
-		htmlmin : {
-			dist : {
-				options: {                                 
-					removeComments: true,
-					collapseWhitespace: true
-				},
-				files: {
-					'index.html': 'main.html'
-				}
-			}
-		},
 		jscs: {
-			src: 'js/src/**/*.js',
+            src: 'js/*.js',
 			options: {
 				config: ".jscsrc"
 			}
@@ -50,8 +35,7 @@ module.exports = function(grunt){
 					outputStyle: 'expanded'
 				},
 				files: {
-					'dist/css/lib.css': 'scss/lib/*.scss',
-					'dist/css/styles.css': 'scss/custom/*.scss'
+					'build/css/style.css': 'scss/main.scss'
 				}
 			},
 			prod: {
@@ -59,23 +43,14 @@ module.exports = function(grunt){
 					outputStyle: 'compressed'
 				},
 				files: {
-					'dist/css/lib.css': 'scss/lib/*.scss',
-					'dist/css/styles.css': 'scss/custom/*.scss'
+					'build/css/style.css': 'scss/main.scss'
 				}
 			}
 		},
 		watch: {
 			scripts: {
-				files: ['js/src/**/*.js', 'scss/**/*.scss', 'partials/*.html', 'main.html'],
+				files: ['js/**/*.js', 'scss/**/*.scss'],
 				tasks: ['dev'],
-			}
-		},
-		express: {
-			server: {
-				options: {
-					port: 3000,
-					bases: '.'
-				}
 			}
 		}
 	});
@@ -86,30 +61,22 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks("grunt-contrib-htmlmin");
     grunt.loadNpmTasks("grunt-jscs");
-	grunt.loadNpmTasks('grunt-uncss');
-	grunt.loadNpmTasks('grunt-express');
 
 
 
 	grunt.registerTask('dev', [
 		'concat',
 		'jscs',
-		'sass:dev',
-		'htmlmin'
+		'sass:dev'
 	]);
-	
-	grunt .registerTask('buildserver', [
-		'dev',
-		'express',
-		'express-keepalive'
-	]);
+    
+    grunt.registerTask('watch', 'dev'); 
 	
 	grunt.registerTask('prod', [
 		'concat',
 		'jscs',
 		'uglify',
-		'sass:prod',
-		'htmlmin'
+		'sass:prod'
 	]);
 	
 	grunt.registerTask('default', ['watch']);
